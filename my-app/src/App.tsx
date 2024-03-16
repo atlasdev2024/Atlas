fdecf729debc40ca72e24ea1a7b30025b5955cf2
rjmoraleda <atlas.dev.2024@gmail.com>
2024-03-16 19:41:29 +0800

    Install dependencies
    Tailwind
    Craco
    TW configuration
    Craco Configuration

---
+++ my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Atlas Premium Auto Shield
+    </div>
+  );
+}
+
+export default App;
