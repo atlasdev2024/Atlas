8ce5240fee4e00609d68583ed8bf2f5422a59fdf
rjmoraleda <atlas.dev.2024@gmail.com>
2024-03-19 19:06:44 +0800

    Dependencies update
    Installed styled-compnents
    Installed twin-macro
    components folder set up

---
+++ my-app/src/app/containers/homepage/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const PageContainer = styled.div`
+
+`;
+
+export function homepage() {
+    return <PageContainer>
+        Atlas Premium Auto Shield
+    </PageContainer>
+}
