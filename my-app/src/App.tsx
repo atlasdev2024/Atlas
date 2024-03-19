877307b90534b109c08779e6f1d5eaf8e6b551e0
rjmoraleda <atlas.dev.2024@gmail.com>
2024-03-19 22:07:26 +0800

    error
    error

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
+import tw from 'twin.macro';
 import './App.css';
+import { HomePage } from './app/containers/HomePage';
+
+const AppContainer = styled.div`
+${`tw
+  w-full
+  h-full
+  flex-col
+
+`};
+`;
+
+
 
 function App() {
-  return (
-    <div className="App">
-      Atlas Premium Auto Shield
-    </div>
-  );
+  return <AppContainer>
+    <HomePage/>
+  </AppContainer>;
 }
 
 export default App;
--- my-app/src/app/containers/homepage/index.tsx
+++ my-app/src/app/containers/homepage/index.tsx
@@ -2,10 +2,18 @@ import React from "react";
 import styled from "styled-components";
 
 const PageContainer = styled.div`
+    ${`tw
+    flex
+    flex-col
+    w-full
+    h-full
+    items-center
+    overflow-x-hidden
 
+`}
 `;
 
-export function homepage() {
+export function HomePage() {
     return <PageContainer>
         Atlas Premium Auto Shield
     </PageContainer>
